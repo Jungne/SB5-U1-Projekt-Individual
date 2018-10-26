b8b3019f45bfcfdc255b54a789dc6cbd47b3e91f
Jungne <jungne7@gmail.com>
2018-10-26 14:44:44 +0200

    Added jsondb package

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require("express")
 var app = express()
+var JsonDB = require('node-json-db')
+var db = new JsonDB("database", true, true);
 
 const port = 3000; 
 
@@ -8,6 +10,15 @@ app.use(express.static('public'));
 //Routes
 app.get('/hello', function (req, res) {
   res.send("hello");
+})
+
+app.get('/dbtest', function (req, res) {
+  res.send(db.getData("/"));
+})
+
+app.post('/newUser', function (req, res) {
+  db.push("/users/" + req.query.username , {"password": req.query.password})
+  res.send("its all good");
 })
 
 //Starts the server
