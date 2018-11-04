96c1dc8e389e990071e2080853a244c430510aea
panil93 <frulauraw@gmail.com>
2018-11-04 21:44:37 +0100

    login alive!

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require("express")
 var app = express()
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var bodyParser = require("body-parser")
 var JsonDB = require('node-json-db')
 var db = new JsonDB("database", true, true);
@@ -9,14 +11,38 @@ const port = process.env.PORT || 5000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');        
+    }
+    next();
+});
+var sessionChecker = (req, res, next) => {
+    if (req.session.user && req.cookies.user_sid) {
+        next();
+    } else {
+        return res.redirect('/signin');
+    }    
+};
 
 //Routes
-app.get('/', function (req, res) {
-  res.sendfile("index.html");
+app.get('/index', sessionChecker,(req, res) => {
+	console.log("index - becase / is taken over by app.user static files");
+  	res.sendfile("index.html");
 })
 
-app.get('/hello', function (req, res) {
-  res.send("hello dudes");
+app.get('/hello',sessionChecker, (req, res) => {
+  	res.send("hello dudes");
 })
 
 app.get('/dbtest', function (req, res) {
@@ -33,6 +59,50 @@ app.get('/getUsers', function (req, res) {
   res.send(db.getData("/users"));
   console.log("List of users requested.")
 })
+app.route('/signin')
+    .get(function (req, res) {
+        res.sendFile(__dirname + '/public/signin.html');
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+            console.log(username +" "+ password);
+            try {
+            var user = db.getData("/users/"+ username);
+            	if(password == user.password){
+            		req.session.user = JSON.stringify(user);
+            		res.redirect('/index.html');
+            	}
+            	else{
+            		res.redirect('/signin');
+            		console.log("Wrong password and/or login. Try again")
+
+            	}
+        	}
+        	catch(error)
+        	{
+        		res.redirect('/signin');
+        		console.log("no redirect. user not found")
+        	}
+       /* User.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });*/
+    });
+    app.get('/logout', (req, res) => {
+    if (req.session.user && req.cookies.user_sid) {
+        res.clearCookie('user_sid');
+        res.redirect('/');
+    } else {
+        res.redirect('/signin');
+    }
+});
 
 //Starts the server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
