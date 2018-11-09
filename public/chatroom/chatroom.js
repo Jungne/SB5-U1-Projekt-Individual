8b64d3183a95d2cc9c5ee9d29ccb5133fa12f749
Nekaerth <lasse.j.lundin@gmail.com>
2018-11-09 15:07:53 +0100

    #5 Basic client websocket test

--- public/chatroom/chatroom.js
+++ public/chatroom/chatroom.js
@@ -1,3 +1,8 @@
-window.onload = function() {
+window.onload = function () {
+    var Socket = new WebSocket("ws://localhost:8080/");
 
+    ws.onopen = function () {
+        console.log("connected succesfully");
+        ws.send("Hello server! - client");
+    };
 };
