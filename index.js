f0508f642e2c23bb1182a7eb0949b83b357405dc
panil93 <frulauraw@gmail.com>
2018-11-09 22:36:40 +0100

    login and signup full functional

--- index.js
+++ index.js
@@ -18,26 +18,26 @@ app.use(bodyParser.json());
 //from: https://www.codementor.io/mayowa.a/how-to-build-a-simple-session-based-authentication-system-with-nodejs-from-scratch-6vn67mcy3?fbclid=IwAR2uT--Fi394bUHW6YO2esKbzqCugODOZptk2Ks7fNxru-KdDhcJ7hww_z8
 app.use(cookieParser());
 app.use(session({
-    key: 'user_sid',
-    secret: 'somerandonstuffs',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
+	key: 'user_sid',
+	secret: 'somerandonstuffs',
+	resave: false,
+	saveUninitialized: false,
+	cookie: {
+		expires: 600000
+	}
 }));
 app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');        
-    }
-    next();
+	if (req.cookies.user_sid && !req.session.user) {
+		res.clearCookie('user_sid');        
+	}
+	next();
 });
 var sessionChecker = (req, res, next) => {
-    if (req.session.user && req.cookies.user_sid) {
-        next();
-    } else {
-        return res.redirect('/signin');
-    }    
+	if (req.session.user && req.cookies.user_sid) {
+		next();
+	} else {
+		return res.redirect('/signin');
+	}    
 };
 
 //Websocket
@@ -50,10 +50,10 @@ wss.on('connection', (ws) => {
       console.log('received: %s', message);
 
       wss.clients.forEach(client => {
-        if (client != ws) {
-            client.send(message);
-        }    
-    });
+      	if (client != ws) {
+      		client.send(message);
+      	}    
+      });
   });
 
   //send immediatly a feedback to the incoming connection    
@@ -65,75 +65,96 @@ wss.on('connection', (ws) => {
 //Routes
 app.get('/index', sessionChecker,(req, res) => {
 	console.log("index - becase / is taken over by app.user static files");
-  	res.sendfile("index.html");
+	res.sendfile("index.html");
 })
 
 app.get('/hello',sessionChecker, (req, res) => {
-  	res.send("hello dudes");
+	res.send("hello dudes");
 })
 
 app.get('/dbtest', function (req, res) {
-  res.send(db.getData("/"));
+	res.send(db.getData("/"));
 })
 
 app.post('/newUser', function (req, res) {
-    db.push("/users/" + req.body.username , {"password": req.body.password})
-    res.send("Created new user.");
-    console.log("Created new user.")
-    log(req.ip, "Created new user: " + req.body.username)
+	db.push("/users/" + req.body.username , {"password": req.body.password})
+	res.send("Created new user.");
+	console.log("Created new user.")
+	log(req.ip, "Created new user: " + req.body.username)
 })
 
 app.get('/getUsers', function (req, res) {
-  res.send(db.getData("/users"));
-  console.log("List of users requested.")
-  log(req.ip, "List of users requested.")
+	res.send(db.getData("/users"));
+	console.log("List of users requested.")
+	log(req.ip, "List of users requested.")
 })
 app.route('/signin')
-    .get(function (req, res) {
-        res.sendFile(__dirname + '/public/signin.html');
-    })
-    .post((req, res) => {
-        var username = req.body.username,
-            password = req.body.password;
-            console.log(username +" "+ password);
-            try {
-            var user = db.getData("/users/"+ username);
-            	if(password == user.password){
-            		req.session.user = JSON.stringify(user);
-            		res.redirect('/index.html');
-            	}
-            	else{
-            		res.redirect('/signin');
-            		console.log("Wrong password and/or login. Try again")
+.get(function (req, res) {
+	res.sendFile(__dirname + '/public/signin.html');
+})
+.post((req, res) => {
+	var username = req.body.username,
+	password = req.body.password;
+	console.log(username +" "+ password);
+	try {
+		var user = db.getData("/users/"+ username);
+		if(password == user.password){
+			req.session.user = JSON.stringify(user);
+			res.redirect('/chatroom/chatroom.html');
+		}
+		else{
+			res.redirect('/signin');
+			console.log("Wrong password and/or login. Try again")
 
-            	}
-        	}
-        	catch(error)
-        	{
-        		res.redirect('/signin');
-        		console.log("no redirect. user not found")
-        	}
-    });
-    app.get('/logout', (req, res) => {
-    if (req.session.user && req.cookies.user_sid) {
-        res.clearCookie('user_sid');
-        res.redirect('/');
-    } else {
-        res.redirect('/signin');
-    }
+		}
+	}
+	catch(error)
+	{
+		res.redirect('/signin');
+		console.log("no redirect. user not found")
+	}
+});
+app.get('/logout', (req, res) => {
+	if (req.session.user && req.cookies.user_sid) {
+		res.clearCookie('user_sid');
+		res.redirect('/');
+	} else {
+		res.redirect('/signin');
+	}
+});
+app.post('/signup',(req, res)=>{
+	try{
+		/*
+		* parsen cannot work without variable
+		*from: https://www.npmjs.com/package/node-json-db
+		*/
+		var newuser = {};
+		newuser[req.body.username] = {"password": req.body.password};
+		console.log(newuser);
+		db.push("/users", newuser,false);
+		
+		var user = db.getData("/users/"+ req.body.username);
+		req.session.user = JSON.stringify(user);
+		res.redirect('/chatroom/chatroom.html');
+	}
+	catch(error){
+		console.log(error);
+		res.redirect('/signup.html');
+
+	}
 });
 
 //Starts the server
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 server.listen(port, () => {
-  console.log(`Server started on port ${server.address().port} :)`);
+	console.log(`Server started on port ${server.address().port} :)`);
 });
 
 //Function for logging data from requests
 function log(ip, desc) {
-  db.push("/logs[]", {
-    "timestamp": new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
-    "ip": ip,
-    "description": desc
-  })
+	db.push("/logs[]", {
+		"timestamp": new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+		"ip": ip,
+		"description": desc
+	})
 }
