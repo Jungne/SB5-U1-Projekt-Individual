109a59af2726d58798e267cd459e52550166ccaa
Nekaerth <lasse.j.lundin@gmail.com>
2018-11-12 13:55:39 +0100

    more visual changes

---
+++ public/signin/signin.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    var loginbutton = document.getElementById("loginbutton")
+    var cancelbutton = document.getElementById("cancelbutton")
+
+    loginbutton.onclick = function () {
+
+    }
+
+    cancelbutton.onclick = function () {
+        window.location.href = "../index.html";
+    }
+
+};
